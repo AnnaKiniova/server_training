bdf05a95d8e321e1a79f77919ec0deadd2ace97c
Anna Kiniova <anna.kiniova@gmail.com>
2020-03-25 23:01:38 +0300

    feat: add user search via id

---
+++ error.js
@@ -0,0 +1,7 @@
+const errors = {
+  "200": { name: "ok", message: "Success" },
+  400: { name: "400", message: "Bad request" },
+  404: { name: "404", message: "User does not exist" }
+};
+
+module.exports.errors = errors;
--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const http = require("http");
+//const url = require("url");
 
 const { users } = require("./users.js");
+const { errors } = require("./error.js");
 
 const server = http.createServer((request, response) => {
   if (request.method === "GET") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(JSON.stringify(users));
-    response.end();
-    console.log(request.method, request.url);
-  } else {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("this method is not supported!");
-    response.end();
-  }
-});
+    // const urlParsed = url.parse(request.url);
+    // console.log(urlParsed);
+    const pathName = request.url.split("/");
+    console.log(pathName);
 
-//response.write('hello')
+    switch (pathName[1]) {
+      case "users":
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.write(JSON.stringify(users));
+        break;
+      case "user": {
+        if (pathName[2]) {
+          const user = users.find(el => el.id === parseInt(pathName[2]));
+          if (user) {
+            response.writeHead(200, { "Content-Type": "application/json" });
+            const { name, description } = user;
+            response.write(JSON.stringify({ name, description }));
+          } else {
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.write(JSON.stringify(errors[404]));
+          }
+        } else {
+          response.writeHead(400, { "Content-Type": "text/html" });
+          response.write(JSON.stringify(errors[400]));
+        }
+        break;
+      }
+      default: {
+        response.writeHead(404, { "Content-Type": "text/html" });
+        response.write(JSON.stringify(errors[404]));
+      }
+    }
+  } else {
+    response.write("this method is not supported!");
+  }
+  response.end();
+});
 server.listen(8080);
